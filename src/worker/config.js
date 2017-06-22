99cb30459f8ab66f26d8e90babbf3a890535a822
Tomasz Szarzynski <tszarzynski@gmail.com>
2017-06-22 11:43:07 +0100

    Redirect and messaging (#3)
    * Resolved conflicts
    
    * Change some variables names

---
+++ src/worker/config.js
@@ -0,0 +1,9 @@
+export const TOKEN_NAME = 'token';
+export const GATEWAY_URL = '/gateway.html';
+export const INDEX_URL = '/test.html';
+
+export const BYPASSED_URLS = [
+    GATEWAY_URL,
+    '/client.js',
+    'browser-sync',
+];
