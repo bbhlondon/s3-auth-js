cb84bee331278e00c4bfb4cf1ed054073dc60c6d
Tomasz Szarzynski <tszarzynski@gmail.com>
2017-06-23 12:21:11 +0100

    Custom IDB implementation. Added some tests. (#7)
    * Added storage tests
    
    * Custom IDB implementation. Added some tests.
    
    * Removed dist files

--- src/worker/storage.js
+++ src/worker/storage.js
@@ -1,32 +1,39 @@
-import 'idb-keyval';
+import * as idb from './../idb';
 import { TOKEN_NAME } from './config';
 
 /**
  * Get stored token
- * 
+ *
  * @export
  * @returns {Promise}
  */
 export function getToken() {
-    return idbKeyval.get(TOKEN_NAME);
+    return idb.get(TOKEN_NAME);
 }
 
 /**
  * Set stored token
- * 
+ *
  * @export
  * @returns {Promise}
  */
 export function setToken(value) {
-    return idbKeyval.set(TOKEN_NAME, value);
+    try {
+        if (!value) throw Error('Value undefined');
+        if (value && typeof value !== 'string') throw Error('Token must be a string');
+
+        return idb.set(TOKEN_NAME, value).then(() => value);
+    } catch (e) {
+        return Promise.reject(e);
+    }
 }
 
 /**
  * Delete stored token
- * 
+ *
  * @export
  * @returns {Promise}
  */
 export function deleteToken() {
-    return idbKeyval.delete(TOKEN_NAME);
+    return idb.delete(TOKEN_NAME);
 }
