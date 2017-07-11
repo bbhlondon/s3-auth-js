b35e4e6758a4ad5f2a78fb548bece41794b8f74c
Tomasz Szarzynski <tszarzynski@gmail.com>
2017-07-11 15:23:22 +0100

    REwrote Logger and added tests (#10)
    * REwrote Logger and added tests
    
    * Fixed package.json

--- src/client/index.js
+++ src/client/index.js
@@ -1,31 +1,29 @@
-import Logger from '../logger/logger';
-
-const logger = new Logger();
+import Logger from '../logger';
 
 export default function registerServiceWorker(swPath) {
     if (!('serviceWorker' in navigator)) {
-        logger.log('[Page] This browser doesn\'t support service workers');
+        Logger.log('[Page] This browser doesn\'t support service workers');
         return false;
     }
 
     if (navigator.serviceWorker.controller) {
         if (navigator.serviceWorker.controller.scriptURL.indexOf(swPath) >= 0) {
-            logger.log('[Client] The service worker is already active');
+            Logger.log('[Client] The service worker is already active');
             
         } else {
-            logger.error(`[Client] The page already has another service worker: ${navigator.serviceWorker.controller.scriptURL}`);
+            Logger.error(`[Client] The page already has another service worker: ${navigator.serviceWorker.controller.scriptURL}`);
         }
         return true;
     }
 
-    logger.log('[Client] The service worker needs to be installed');
+    Logger.log('[Client] The service worker needs to be installed');
     window.addEventListener('load', () => {
         navigator.serviceWorker.register(swPath).then((registration) => {
             // Registration was successful
-            logger.log(`[Client] ServiceWorker registration successful with scope: ${registration.scope}`);
+            Logger.log(`[Client] ServiceWorker registration successful with scope: ${registration.scope}`);
         }, (err) => {
             // registration failed :(
-            logger.log(`[Client] ServiceWorker registration failed: ${err}`);
+            Logger.log(`[Client] ServiceWorker registration failed: ${err}`);
         });
     });
     return true;
---
+++ src/logger/_index.js
@@ -0,0 +1,7 @@
+export default function canLog() {
+    try {
+        return ENV !== 'production';
+    } catch (e) {
+        return true;
+    }
+}
