14ad07e64f196ba809ac4e301e9686adfe02ed0c
Tomasz Szarzynski <tszarzynski@gmail.com>
2017-07-20 12:22:02 +0100

    Fixed root level tests (#18)
    * Fixed root level tests
    
    * Removed keep-open

--- src/utils.test.js
+++ src/utils.test.js
@@ -6,13 +6,15 @@ import { ERROR_PARAM_REQUIRED } from './consts';
 
 test('makeMessgae throws error when type undefined', (t) => {
     t.plan(1);
-    t.throws(() => utils.makeMessage(), ERROR_PARAM_REQUIRED);
+    // @ts-ignore
+    t.throws(() => utils.default(), ERROR_PARAM_REQUIRED);
 });
 
 test('makeMessgae returns correct message object', (t) => {
     t.plan(1);
     const expected = { type: 'test', payload: { test: 'test' } };
-    const msg = utils.makeMessage('test', { test: 'test' });
+    const msg = utils.default('test', { test: 'test' });
 
     t.deepLooseEqual(expected, msg);
 });
+
