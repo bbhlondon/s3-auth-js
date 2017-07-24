2934d3a5ed1ba5ddf6387d6c0ea346049d347ef1
Tomasz Szarzynski <tomasz.szarzynski@bbh.co.uk>
2017-07-24 12:30:00 +0100

    Added auth to responses

--- src/worker/responses.test.js
+++ src/worker/responses.test.js
@@ -1,6 +1,8 @@
 import test from 'tape';
+import sinon from 'sinon';
 import { respondWithRedirectToGateway, respondWithRequestedItem, repondWithRedirectToIndex } from './responses';
 import { GATEWAY_URL, INDEX_URL } from '../consts';
+import * as state from './state';
 
 test('respondWithRedirectToGateway redirects to gateway page', (t) => {
     const event = { type: 'fetch', request: new Request(window.location.href) };
@@ -24,8 +26,12 @@ test('respondWithRequestedItem responds with requested item', (t) => {
     const event = { type: 'fetch', request: new Request(window.location.href) };
     const expected = event.request.url;
 
+    const stub = sinon.stub(state, 'getCredentials').returns({ key: 'key', secret: 'secret' });
+
     t.plan(1);
     const request = respondWithRequestedItem(event);
     t.equal(request.url, expected);
+
+    stub.restore();
 });
 
